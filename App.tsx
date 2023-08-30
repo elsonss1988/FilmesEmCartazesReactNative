803d362db6927a513b63cf746bbe781496b99577
Elson <elson@Elsons-MacBook-Pro.local>
2023-08-30 18:13:20 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Button,
+  FlatList,
+  Image,
+  Alert,
+} from "react-native";
+import { styles } from "./styles";
 
 export default function App() {
+  type Props = {
+    titulo: string;
+    avatar: string;
+  };
+
+  const [movies, setMovies] = useState<Props[]>([]);
+
+  const handleLoadButton = async () => {
+    const req = await fetch("https:api.b7web.com.br/cinema");
+    const json = await req.json();
+
+    if (json) {
+      setMovies(json);
+    }
+
+    Alert.alert("Update", "Pegou " + json.length + " filmes!");
+    movies.forEach((i) => console.log(i));
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="Carregar Filmes" onPress={handleLoadButton} />
+      <Text style={styles.totalMovieText}>
+        {" "}
+        Total de filmes: {movies.length}
+      </Text>
+      <FlatList
+        style={styles.list}
+        data={movies}
+        renderItem={({ item }) => (
+          <View style={styles.movieItem}>
+            <Image
+              source={{ uri: item.avatar }}
+              style={styles.movieImage}
+              resizeMode="contain"
+            />
+            <Text style={styles.movieTitle}>{item.titulo}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.titulo}
+      />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#333",
+  },
+  totalMovieText: {
+    color: "#FFF",
+    fontSize: 18,
+    textAlign: "center",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  list: {
+    flex: 1,
+  },
+  movieItem: {
+    marginBottom: 30,
+  },
+  movieImage: {
+    height: 400,
+    borderRadius: 100,
+  },
+  movieTitle: {
+    color: "#FFF",
+    fontSize: 24,
+    textAlign: "center",
+    marginTop: 5,
+  },
+});
